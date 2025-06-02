a8bfcb017debe925de3bcbeabfc2ec94ac1efea0
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-02 11:09:38 +0200

    Refactored load map function and getWeather

--- js/script.js
+++ js/script.js
@@ -2,70 +2,58 @@
 
 const API_KEY = '0b5b56156df53ef0a4e58adb399b776e';
 
-const getWeather = function (country) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Goma&appid=50979477a02a607dccd7a9abac59199b&units=metric`
-  )
-    .then(function (response) {
-      console.log(response);
-      if (!response.ok) throw new Error('Weather data fetch failed.');
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.main.temp);
-      const temperature = data;
-      document.querySelector('.feels--like--span').textContent = Math.trunc(
-        data.main.feels_like
-      );
-      document.querySelector('.highest--temperature').textContent = Math.trunc(
-        data.main.tem_max
-      );
-      document.querySelector('.lowest--temperature').textContent = Math.trunc(
-        data.main.tem_min
-      );
-      document.querySelector('.humidity--percent').textContent = Math.trunc(
-        data.main.humidity
-      );
-      document.querySelector('.temp').textContent = Math.trunc(data.main.temp);
-      document.querySelector('.city').textContent = data.name;
-      document.querySelector('.weather').textContent =
-        data.weather[0].description;
-      document.querySelector('.wind--span').textContent = Math.trunc(
-        data.wind.speed
-      );
-      //   document.querySelector('.gusts--span').textContent = Math.trunc(
-      // data.wind.gusts
-      //   );
-      document.querySelector('.directtion--span').textContent = Math.trunc(
-        data.wind.deg
-      );
-      console.log(temperature);
-    })
-    .catch((err) => console.error('Error💣: ', err.message));
+const updateText = function (selector, value) {
+  const element = document.querySelector(selector);
+  if (element) element.textContent = value;
 };
 
-getWeather('drc');
+const loadMap = function () {
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const { latitude, longitude } = position.coords;
+        const coords = [latitude, longitude];
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
-      console.log(coords);
+        const map = L.map('map').setView(coords, 12);
 
-      const map = L.map('map').setView(coords, 12);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+      },
+      function () {
+        alert('Failed to load your current location.');
+      }
+    );
+};
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
-    },
-    function () {
-      alert('Failed to load your current location.');
-    }
-  );
+const getWeather = async function (city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=50979477a02a607dccd7a9abac59199b&units=metric`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Weather data fetch failed.');
+
+    const data = await response.json();
+
+    updateText('.feels--like--span', Math.round(data.main.feels_like));
+    updateText('.highest--temperature', Math.round(data.main.temp_max));
+    updateText('.lowest--temperature', Math.round(data.main.temp_min));
+    updateText('.humidity--percent', Math.round(data.main.humidity));
+    updateText('.temp', Math.round(data.main.temp));
+    updateText('.city', data.name);
+    updateText('.weather', data.weather[0].description);
+    updateText('.wind--span', Math.round(data.wind.speed));
+    updateText('.gusts--span', Math.round(data.wind.gusts));
+    updateText('.direction--span', Math.round(data.wind.deg));
+  } catch (error) {
+    console.error('Error💣: ', err.message);
+  }
+};
+
+getWeather('Goma');
+loadMap();
